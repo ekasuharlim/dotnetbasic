5bd69c991d12f2e8bf3a453b0881156b4bb99c7d
eka <eka@example.com>
2021-07-28 07:17:43 +0800

    base

---
+++ disample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace disample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dependency = new DependencyServices();
+            var service  = (HelloMessageService)dependency.CreateService(typeof(HelloMessageService));
+            MessageConsumer consumer = new MessageConsumer(service);
+            consumer.DisplayGreetings();
+            Console.WriteLine("Done");
+        }
+    }
+
+    public class DependencyServices{
+
+        public object CreateService(Type t){
+            return Activator.CreateInstance(t);
+
+        }
+    }
+
+    public class MessageConsumer{
+
+        HelloMessageService _service;
+        public MessageConsumer(HelloMessageService service){
+            _service = service;
+        }
+
+        public void DisplayGreetings(){
+            _service.ShowMessage();
+        }        
+    }
+
+    public class HelloMessageService
+    {
+        public HelloMessageService(){
+
+        }
+
+        public void ShowMessage(){
+            Console.WriteLine("Hello there");
+        }
+
+    }
+}
