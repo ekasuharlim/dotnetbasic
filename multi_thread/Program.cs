7298b1521a0cdf9feb63b29f1f6351e381b20a93
eka <eka@example.com>
2025-03-11 15:21:18 +0700

    first excercise, using join, to wait for the thread to finish

---
+++ multi_thread/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+void PrintNumbers(string threadName, int sleepTime) {
+    for (int i = 0; i < 5; i++) {
+        Console.WriteLine($"Thread {threadName} - Number: {i}");
+        Thread.Sleep(sleepTime);
+    }
+}
+
+Thread t1 = new Thread(() => PrintNumbers("Thread 1", 1000));
+Thread t2 = new Thread(() => PrintNumbers("Thread 2", 500));
+Thread t3 = new Thread(() => PrintNumbers("Thread 3", 100));
+
+t1.Start();
+t2.Start();
+t3.Start();
+
+t1.Join();
+t2.Join();
+t3.Join();
+
+
+Console.WriteLine("Done");
+Console.ReadKey();
+
+
